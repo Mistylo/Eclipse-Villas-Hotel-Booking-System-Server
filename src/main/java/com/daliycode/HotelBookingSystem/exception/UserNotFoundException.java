7eafaa7c00df4c719f0a972810d5b81cf8772417
LeticiaChen <105430183+Mistylo@users.noreply.github.com>
2024-12-25 10:44:51 +0000

    Update UserNotFoundException.java

--- src/main/java/com/daliycode/HotelBookingSystem/exception/UserNotFoundException.java
+++ src/main/java/com/daliycode/HotelBookingSystem/exception/UserNotFoundException.java
@@ -2,7 +2,6 @@ package com.daliycode.HotelBookingSystem.exception;
 
 public class UserNotFoundException extends RuntimeException {
 
-    // 构造函数
     public UserNotFoundException(String message) {
         super(message);
     }
